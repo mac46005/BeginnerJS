0e7bb343ef8d0945a398bc7b5956997a69b88384
Marco Preciado <mac46005@gmail.com>
2021-04-06 20:23:18 -0500

    Checker Board

--- Chapter1.js
+++ Chapter1.js
@@ -2,7 +2,7 @@
 
 //Chapter 1 Excercises
 
-//1. Looping Triangle
+// 1-1 Looping Triangle
 //Write a loop that makes seven calls to console.log to output the following
 //#
 //##
@@ -23,7 +23,7 @@ for (let x = 0; x < 7; x++){
 console.log();
 
 
-//2.FizzBuzz
+// 1-2 FizzBuzz
 //Write a program that uses console.log to print
 //all the numbers from 1 to 100, with two exceptions.
 //For numbers divisible by 3 print "Fizz"
@@ -33,15 +33,32 @@ let f = "Fizz";
 let b = "Buzz";
 for (let x = 0; x < 101; x++){
     if ((x % 3 == 0) && (x % 5 == 0)) {
-        console.log(f + b);
+        //console.log(f + b);
     }
     else if(x % 3 == 0){
-        console.log(f);
+        //console.log(f);
     }
     else if(x % 5 == 0){
-        console.log(b);
+        //console.log(b);
     }
     else{
-        console.log(x)
+        //console.log(x)
     }
 }
+
+//1-3 ChessBoard
+let board = "";
+let size = 8;
+for(x = 0; x < size; x++){
+    for(y = 0; y < size; y++){
+        if((x + y) % 2 == 0){
+            board += "#";
+        }
+        else{
+            board += " ";
+        }
+    }
+    board += "\n";
+}
+console.log(board);
+
