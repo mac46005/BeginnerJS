46fd06f4f45b941f3fff0a93d9fc42816db35b25
Marco Preciado <mac46005@gmail.com>
2021-04-06 21:29:45 -0500

    Char Count

--- Chapter2.js
+++ Chapter2.js
@@ -33,3 +33,15 @@ console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
 
+// 2-3 Char count
+let wrd = "Baboon Bitch"
+function countChar(word,char){
+    let count = 0;
+    for(x = 0; x < wrd.length; x++){
+        if(word[x] == char){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countChar(wrd,'B'));
