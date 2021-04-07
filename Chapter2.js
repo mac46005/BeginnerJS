377bd32fa7140197d1193cd20077279cdee9c0b6
Marco Preciado <mac46005@gmail.com>
2021-04-06 21:14:04 -0500

    Recursion excersize

--- Chapter2.js
+++ Chapter2.js
@@ -1,4 +1,4 @@
-//Minimum
+// 2-1 Minimum
 function min(x,y){
     if(y > x){
         return y;
@@ -9,3 +9,27 @@ function min(x,y){
 }
 
 console.log(min(10,3));
+
+// 2-2 Recursion
+//Define a recursive function that corresponds to the following
+//Zero is even
+//One is odd
+//For any other number N, its evenness is the same as N-2
+function isEven(x){
+    if(x == 0){
+        return true;
+    }
+    else if(x == 1){
+        return false;
+    }
+    else if(x < 0){
+        return isEven(-x);
+    }
+    else{
+        return isEven(x - 2);
+    }
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
