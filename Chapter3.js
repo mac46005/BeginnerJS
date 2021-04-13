8d93290d721f3725853e5fba9b5f16b8bdc7338e
Marco Preciado <mac46005@gmail.com>
2021-04-12 21:05:52 -0500

    IsEven

--- Chapter3.js
+++ Chapter3.js
@@ -9,3 +9,22 @@ function min(x,y){
 }
 
 console.log(min(4,5));
+
+// 3-2 ISEVEN
+
+function isEven(n){
+    if(n == 0){
+        return true;
+    }
+    else if(n == 1){
+        return false;
+    }
+    else if(n < 0){
+        return isEven(-n);
+    }
+    else{
+        return isEven(n-2);
+    }
+}
+console.log(isEven(50));
+console.log(isEven(75));
