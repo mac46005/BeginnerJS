b6e6f8a7f85a657cef4cb5301f6024a1be17a9cd
Marco Preciado <mac46005@gmail.com>
2021-04-12 21:56:03 -0500

    range has bug: cant count down.

--- Chapter4.js
+++ Chapter4.js
@@ -1,14 +1,18 @@
 // Chapter 4 Excersizes
 
 // 4-1 The Sum of a range
-function range(start,end){
+function range(start,end,step = 1){
+    let count = start;
     let array = [];
-    for(x = start; x < end + 1; x++){
-        array.push(x)
+    while (count < end + 1) {
+        array.push(count);
+        count += step;
     }
     return array;
 }
 console.log(range(3,9));
+
+console.log(range(10,4,-1));
 
 function sum(array){
     let s = 0;
@@ -17,4 +21,4 @@ function sum(array){
     }
     return s;
 }
-console.log(sum(range(3,9)));
+//console.log(sum(range(3,9)));
