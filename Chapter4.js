bb5fdab7919effbb1c2acd11d52b7763bf9b5905
Marco Preciado <mac46005@gmail.com>
2021-04-12 21:18:47 -0500

    range

--- Chapter4.js
+++ Chapter4.js
@@ -1,1 +1,11 @@
 // Chapter 4 Excersizes
+
+// 4-1 The Sum of a range
+function range(start,end){
+    let array = [];
+    for(x = start; x < end + 1; x++){
+        array.push(x)
+    }
+    return array;
+}
+console.log(range(3,9));
