2a427e7f89da2f96a4cf6d5c26acd4561d7fd9ef
Marco Preciado <mac46005@gmail.com>
2021-05-11 00:11:52 -0500

    Chpt5 self contained vs external functions

---
+++ Chapter5.js
@@ -0,0 +1,24 @@
+let total = 0,count = 1;
+while(count <= 1){
+  total += count;
+  count += 1;
+}
+console.log(total);
+
+console.log(sum(range(1,10)))
+
+function sum(array){
+  let result = 0;
+ for(let x in array){
+   result += x;
+ } 
+ return result;
+}
+
+function range(start,end){
+  let array = [];
+  for (var i = start; i < end; i++){
+  array.push(i);
+  }
+  return array;
+}
