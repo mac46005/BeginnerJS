540f8f4aaa6557ab63e8c6299d7158aa56707d86
Marco Preciado <mac46005@gmail.com>
2021-05-28 16:12:02 -0400

    Object prototype

--- Chapter6.js
+++ Chapter6.js
@@ -32,3 +32,8 @@ let empty = {};
 console.log(empty.toString);
 console.log(empty.toString());
 
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+
+console.log(Object.getPrototypeOf(Object.prototype));
+
+
