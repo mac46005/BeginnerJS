8a8d838a45a305416b88a88a3b9ca92ed56d7114
Marco Preciado <mac46005@gmail.com>
2021-05-29 16:09:21 -0400

    new to instantiate object

--- Chapter6.js
+++ Chapter6.js
@@ -50,3 +50,22 @@ let protoRabbit = {
 let killerRabbit = Object.create(protoRabbit);
 killerRabbit.type = "killer";
 killerRabbit.speak("SKREEEEEEEE BITCH");
+
+
+/////////////////////////////////////////////////
+// Classes
+
+function makeRabbit(type){
+    let rabbit = Object.create(protoRabbit);
+    rabbit.type = type;
+    return rabbit;
+}
+
+function Rabbit(type){
+    this.type = type;
+}
+Rabbit.prototype.speak = function(line){
+    console.log(`The ${this.type} rabbit says '${line}'`);
+};
+
+let weirdRabbit = new Rabbit("weird");
