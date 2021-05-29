91342f3d2db56514a77f922a590dd1b1b87bda4e
Marco Preciado <mac46005@gmail.com>
2021-05-29 10:54:46 -0400

    A property like speak(line) in an object expression is a shorthand way of defining a method.

--- Chapter6.js
+++ Chapter6.js
@@ -39,3 +39,14 @@ console.log(Object.getPrototypeOf(Object.prototype));
 console.log(Object.getPrototypeOf(Math.max) == Function.prototype);
 
 console.log(Object.getPrototypeOf([]) == Array.prototype);
+
+let protoRabbit = {
+    speak(line){
+        console.log(`The ${this.type} rabbit says '${line}'`)
+    }
+}
+
+
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEEEEEE BITCH");
