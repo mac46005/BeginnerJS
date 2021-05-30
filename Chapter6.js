4e700c54f37fe3a33f1d2be823a226221504e89f
Marco Preciado <mac46005@gmail.com>
2021-05-30 11:29:09 -0400

    Object.keys returns only an object's own keys, not those in the prototype. As an alternative to the 'in' operator, you can use the 'hasOwnProperty' method, which ignores the object's prototype.

--- Chapter6.js
+++ Chapter6.js
@@ -129,7 +129,9 @@ ages.set("Boris", 39);
 ages.set("Liang",22);
 ages.set("Julia",62);
 
-console.log(`Julia is ${ages.get("Julia")}`);
-console.log("Is Jack's age known?",ages.has("Jack"));
-console.log(ages.has("toString"));
+//console.log(`Julia is ${ages.get("Julia")}`);
+//console.log("Is Jack's age known?",ages.has("Jack"));
+//console.log(ages.has("toString"));
 
+console.log({x: 1}.hasOwnProperty("x"));
+console.log({x: 1}.hasOwnProperty("toString"));
