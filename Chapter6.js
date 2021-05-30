6f178f3ae97855da2a17b3376370f4381e7eaac3
Marco Preciado <mac46005@gmail.com>
2021-05-30 15:50:49 -0400

    Using square brackets to get property values or function values

--- Chapter6.js
+++ Chapter6.js
@@ -156,5 +156,11 @@ Array.prototype[toStringSymbol] = function(){
     return `${this.length} cm of blue yarn`;
 };
 
-console.log([1,2].toString());
-console.log([1,2][toStringSymbol]());
+//console.log([1,2].toString());
+//console.log([1,2][toStringSymbol]());
+
+let stringObject = {
+    [toStringSymbol](){return "a jute rope";}
+};
+
+console.log(stringObject[toStringSymbol]());
