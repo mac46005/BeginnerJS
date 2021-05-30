87fa05d7b4b74c3aae553aedee95fa9cc97ce6a5
Marco Preciado <mac46005@gmail.com>
2021-05-30 11:23:23 -0400

    Methods set get has are part of the interface of the Map object.Quickly update and search a large set of values

--- Chapter6.js
+++ Chapter6.js
@@ -122,5 +122,14 @@ let ages = {
 //console.log("Is Jack's age known?","Jack" in ages);
 //console.log("Is toString's afe known?","toString" in ages);
 
-console.log("toString" in Object.create(null));
-5
+//console.log("toString" in Object.create(null));
+
+ages = new Map();
+ages.set("Boris", 39);
+ages.set("Liang",22);
+ages.set("Julia",62);
+
+console.log(`Julia is ${ages.get("Julia")}`);
+console.log("Is Jack's age known?",ages.has("Jack"));
+console.log(ages.has("toString"));
+
