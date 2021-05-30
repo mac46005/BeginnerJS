c4da30f9fe3268c72a14b679ef1a56bd197a37f8
Marco Preciado <mac46005@gmail.com>
2021-05-30 15:48:10 -0400

    It is possible to include symbol properties in object expressions and classes by using square brackets around the property name. That causes the property name to be evaluated, much like the square bracket proerty access notation.

--- Chapter6.js
+++ Chapter6.js
@@ -141,12 +141,20 @@ ages.set("Julia",62);
 RabbitModel.prototype.toString = function(){
     return `a ${this.type} rabbit`;
 }
-console.log(String(blackRabbit));
+//console.log(String(blackRabbit));
 
 /////////////////////////////////////////////////////////////////
 // Symbols
 
 let sym = Symbol("name");
-console.log(sym == Symbol("name"));
-Rabbit.prototype[sym] == 55;
-console.log(blackRabbit[sym]);
+//console.log(sym == Symbol("name"));
+RabbitModel.prototype[sym] == 55;
+//console.log(blackRabbit[sym]);
+
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function(){
+    return `${this.length} cm of blue yarn`;
+};
+
+console.log([1,2].toString());
+console.log([1,2][toStringSymbol]());
