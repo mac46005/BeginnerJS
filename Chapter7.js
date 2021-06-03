b8a56eb69dc29fcf812cc9743478d0aadc3765bd
Marco Preciado <mac46005@gmail.com>
2021-06-03 16:48:29 -0400

    testing the move method

--- Chapter7.js
+++ Chapter7.js
@@ -1,27 +1,27 @@
 const roads = [
-    "Alice's House-Bob's House","Alice's House-Cabin",
-    "Alice's Houst-Post Office","Bob's House-Town Hall",
-    "Daria's House-Ernie's House","Daria's House-Town Hall",
-    "Ernie's House-Grete's House","Grete's House-Farm",
-    "Grete's House-Shop","Marketplace-Farm",
-    "Marketplace-Post Office","Marketplace-Shop",
-    "Marketplace-Town Hall","Shop-Town"
+    "Alice's House-Bob's House", "Alice's House-Cabin",
+    "Alice's Houst-Post Office", "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop", "Marketplace-Farm",
+    "Marketplace-Post Office", "Marketplace-Shop",
+    "Marketplace-Town Hall", "Shop-Town Hall"
 ]
 
 
-function buildGraph(edges){
+function buildGraph(edges) {
     let graph = Object.create(null);
-    function addEdge(from,to){
-        if(graph[from] == null){
+    function addEdge(from, to) {
+        if (graph[from] == null) {
             graph[from] = [to];
         }
-        else{
+        else {
             graph[from].push(to);
         }
     }
-    for(let[from,to] of edges.map(r => r.split("-"))){
-        addEdge(from,to);
-        addEdge(to,from);
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
     }
     return graph;
 }
@@ -29,24 +29,38 @@ function buildGraph(edges){
 
 const roadGraph = buildGraph(roads);
 
-class VillageState{
-    constructor(place,parcels){
+class VillageState {
+    constructor(place, parcels) {
         this.place = place;
         this.parcels = parcels;
     }
 
-    move(destination){
-        if(!roadGraph[this.place].includes(destination)){
+    move(destination) {
+        if (!roadGraph[this.place].includes(destination)) {
             return this;
         }
-        else{
+        else {
             let parcels = this.parcels.map(p => {
-                if(p.place != this.place){
+                if (p.place != this.place) {
                     return p;
                 }
-                return {place: destination, address: p.address}
+                return { place: destination, address: p.address }
             }).filter(p => p.place != p.address);
-            return new VillageState(destination,parcels);
+            return new VillageState(destination, parcels);
         }
     }
 }
+
+let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office",address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
+
+
+let object = Object.freeze({value:5});
+object.value = 10;
+console.log(object.value);
